018eb3d02517c6d57fd56bf5b7b4fd34666c4792
Atavachron <vladimir.kalichuk@gmail.com>
2019-11-30 00:28:07 +0400

    Added delete functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import { thisExpression } from "@babel/types";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   // renderTags() {
@@ -39,7 +39,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
